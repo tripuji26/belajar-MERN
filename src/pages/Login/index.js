9e3e3622480d0be68cdccdfb15b47798c63b3d3e
tripuji26 <tripujiseptiawan507@gmail.com>
2020-08-30 20:04:59 +0700

    mebuat halaman Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { LoginBg } from "../../assets";
+import { Input, Button, Link, Gap } from "../../components";
 
 const Login = () => {
-    return (
-        <div>
-            <p>page login</p>
-        </div>
-    )
-}
+  return (
+    <div className="main-page">
+      <div className="left">
+        <img src={LoginBg} className="bg-image" alt="imageBg" />
+      </div>
+      <div className="right">
+        <p className="title">Login</p>
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Login" />
+        <Gap height={100} />
+        <Link title="Belum Punya Akun, Silahkan Daftar" />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
