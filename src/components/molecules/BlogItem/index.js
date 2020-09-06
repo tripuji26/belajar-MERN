0ebc22a820cd3d60e79b96e526698e939e4032c4
tripuji26 <tripujiseptiawan507@gmail.com>
2020-09-06 15:40:14 +0700

    membuat styling pada item blog post

---
+++ src/components/molecules/BlogItem/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./blogitem.scss";
+
+const BlogItem = () => {
+  return (
+    <div>
+      <p>Component Blog</p>
+    </div>
+  );
+};
+
+export default BlogItem;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import "./home.scss";
+import { Button, BlogItem } from "../../components";
 
 const Home = () => {
-    return (
-        <div>
-            <p>page home</p>
-        </div>
-    )
-}
+  return (
+    <div className="main-app-wrapper ">
+      <div className="create-wrapper">
+        <Button title="create blog" />
+      </div>
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
+      <BlogItem />
+      <p>pagination</p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
